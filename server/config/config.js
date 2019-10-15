eadb0959a199eb734c1a0bf9b0340256f0efcdbb
Guillermo <guillesotoblanco@gmail.com>
2019-10-15 15:02:38 -0600

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ let urlDB;
 //if (process.env.NODE_ENV === 'dev') {
 //    urlDB = 'mongodb://localhost:27017/cafe';
 //} else {
-    urlDB = 'mongodb+srv://gu99soto:HJEh93VaTpbuRnLQ@cluster0-monml.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI;
 //}
 
 process.env.URLDB = urlDB;
